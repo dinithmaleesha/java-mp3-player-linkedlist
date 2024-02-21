3b59316af398299e94c26e7456913791f22580c7
DMaleesha <maleesha.live123@gmail.com>
2024-02-21 22:17:05 +0530

    Initial commit

---
+++ V3/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace V3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.songList = new System.Windows.Forms.ListBox();
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            this.btn_Previous = new System.Windows.Forms.Button();
+            this.btn_PlayPause = new System.Windows.Forms.Button();
+            this.btn_Next = new System.Windows.Forms.Button();
+            this.lbl_SongName = new System.Windows.Forms.Label();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.btn_AddQueue = new System.Windows.Forms.Button();
+            this.btn_RemoveSong = new System.Windows.Forms.Button();
+            this.lbl_TotalDuration = new System.Windows.Forms.Label();
+            this.lbl_CurrentPosition = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // songList
+            // 
+            this.songList.FormattingEnabled = true;
+            this.songList.ItemHeight = 16;
+            this.songList.Location = new System.Drawing.Point(168, 12);
+            this.songList.Name = "songList";
+            this.songList.Size = new System.Drawing.Size(430, 196);
+            this.songList.TabIndex = 0;
+            this.songList.SelectedIndexChanged += new System.EventHandler(this.songList_SelectedIndexChanged);
+            // 
+            // trackBar
+            // 
+            this.trackBar.LargeChange = 1;
+            this.trackBar.Location = new System.Drawing.Point(168, 281);
+            this.trackBar.Maximum = 1000;
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Size = new System.Drawing.Size(430, 56);
+            this.trackBar.TabIndex = 1;
+            // 
+            // btn_Previous
+            // 
+            this.btn_Previous.Location = new System.Drawing.Point(168, 352);
+            this.btn_Previous.Name = "btn_Previous";
+            this.btn_Previous.Size = new System.Drawing.Size(75, 35);
+            this.btn_Previous.TabIndex = 2;
+            this.btn_Previous.Text = "Previous";
+            this.btn_Previous.UseVisualStyleBackColor = true;
+            this.btn_Previous.Click += new System.EventHandler(this.btn_Previous_Click);
+            // 
+            // btn_PlayPause
+            // 
+            this.btn_PlayPause.Location = new System.Drawing.Point(336, 352);
+            this.btn_PlayPause.Name = "btn_PlayPause";
+            this.btn_PlayPause.Size = new System.Drawing.Size(75, 35);
+            this.btn_PlayPause.TabIndex = 2;
+            this.btn_PlayPause.Text = "Pause";
+            this.btn_PlayPause.UseVisualStyleBackColor = true;
+            this.btn_PlayPause.Click += new System.EventHandler(this.btn_PlayPause_Click);
+            // 
+            // btn_Next
+            // 
+            this.btn_Next.Location = new System.Drawing.Point(523, 352);
+            this.btn_Next.Name = "btn_Next";
+            this.btn_Next.Size = new System.Drawing.Size(75, 35);
+            this.btn_Next.TabIndex = 2;
+            this.btn_Next.Text = " ";
+            this.btn_Next.UseVisualStyleBackColor = true;
+            this.btn_Next.Click += new System.EventHandler(this.btn_Next_Click);
+            // 
+            // lbl_SongName
+            // 
+            this.lbl_SongName.AutoSize = true;
+            this.lbl_SongName.Location = new System.Drawing.Point(168, 238);
+            this.lbl_SongName.Name = "lbl_SongName";
+            this.lbl_SongName.Size = new System.Drawing.Size(46, 17);
+            this.lbl_SongName.TabIndex = 3;
+            this.lbl_SongName.Text = "label1";
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.Location = new System.Drawing.Point(604, 12);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(130, 35);
+            this.btn_Add.TabIndex = 4;
+            this.btn_Add.Text = "Add Music";
+            this.btn_Add.UseVisualStyleBackColor = true;
+            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
+            // 
+            // btn_AddQueue
+            // 
+            this.btn_AddQueue.Location = new System.Drawing.Point(604, 91);
+            this.btn_AddQueue.Name = "btn_AddQueue";
+            this.btn_AddQueue.Size = new System.Drawing.Size(130, 35);
+            this.btn_AddQueue.TabIndex = 5;
+            this.btn_AddQueue.Text = "Add Queue";
+            this.btn_AddQueue.UseVisualStyleBackColor = true;
+            this.btn_AddQueue.Click += new System.EventHandler(this.btn_AddQueue_Click);
+            // 
+            // btn_RemoveSong
+            // 
+            this.btn_RemoveSong.Location = new System.Drawing.Point(604, 173);
+            this.btn_RemoveSong.Name = "btn_RemoveSong";
+            this.btn_RemoveSong.Size = new System.Drawing.Size(130, 35);
+            this.btn_RemoveSong.TabIndex = 6;
+            this.btn_RemoveSong.Text = "Remove Song";
+            this.btn_RemoveSong.UseVisualStyleBackColor = true;
+            this.btn_RemoveSong.Click += new System.EventHandler(this.btn_RemoveSong_Click);
+            // 
+            // lbl_TotalDuration
+            // 
+            this.lbl_TotalDuration.AutoSize = true;
+            this.lbl_TotalDuration.Location = new System.Drawing.Point(605, 281);
+            this.lbl_TotalDuration.Name = "lbl_TotalDuration";
+            this.lbl_TotalDuration.Size = new System.Drawing.Size(46, 17);
+            this.lbl_TotalDuration.TabIndex = 7;
+            this.lbl_TotalDuration.Text = "label1";
+            // 
+            // lbl_CurrentPosition
+            // 
+            this.lbl_CurrentPosition.AutoSize = true;
+            this.lbl_CurrentPosition.Location = new System.Drawing.Point(116, 281);
+            this.lbl_CurrentPosition.Name = "lbl_CurrentPosition";
+            this.lbl_CurrentPosition.Size = new System.Drawing.Size(46, 17);
+            this.lbl_CurrentPosition.TabIndex = 8;
+            this.lbl_CurrentPosition.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(826, 450);
+            this.Controls.Add(this.lbl_CurrentPosition);
+            this.Controls.Add(this.lbl_TotalDuration);
+            this.Controls.Add(this.btn_RemoveSong);
+            this.Controls.Add(this.btn_AddQueue);
+            this.Controls.Add(this.btn_Add);
+            this.Controls.Add(this.lbl_SongName);
+            this.Controls.Add(this.btn_Next);
+            this.Controls.Add(this.btn_PlayPause);
+            this.Controls.Add(this.btn_Previous);
+            this.Controls.Add(this.trackBar);
+            this.Controls.Add(this.songList);
+            this.Name = "Form1";
+            this.Text = "Mp3 Player";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox songList;
+        private System.Windows.Forms.TrackBar trackBar;
+        private System.Windows.Forms.Button btn_Previous;
+        private System.Windows.Forms.Button btn_PlayPause;
+        private System.Windows.Forms.Button btn_Next;
+        private System.Windows.Forms.Label lbl_SongName;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.Button btn_AddQueue;
+        private System.Windows.Forms.Button btn_RemoveSong;
+        private System.Windows.Forms.Label lbl_TotalDuration;
+        private System.Windows.Forms.Label lbl_CurrentPosition;
+    }
+}
+
---
+++ V3/Form1.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+
+namespace V3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Node head = null;
+        private Node currentSong;
+        private Node tail;
+        private WaveOutEvent waveOut;
+        private AudioFileReader audioFile;
+        private Timer trackBarUpdateTimer;
+        private bool isPaused;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lbl_SongName.Text = "Please Add Music";
+            lbl_CurrentPosition.Text = "00:00";
+            lbl_TotalDuration.Text = "00:00";
+        } 
+        class Node
+        {
+            public String name;
+            public String path;
+            public Node prev;
+            public Node next;
+        }
+
+        private void btn_Add_Click(object sender, EventArgs e)              // Add Music
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "MP3 Files (*.mp3)|*.mp3";
+            openFileDialog.Multiselect = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string filePath in openFileDialog.FileNames)
+                {
+                    string fileName = Path.GetFileName(filePath);
+
+                    InsertAtEnd(fileName, filePath);
+                    songList.Items.Add(fileName);
+                }
+            }
+        }
+        private void btn_AddQueue_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "MP3 Files (*.mp3)|*.mp3";
+            openFileDialog.Multiselect = false;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string currentlyPlayingSong = songList.SelectedItem?.ToString();
+
+                if (currentlyPlayingSong != null)
+                {
+                    foreach (string filePath in openFileDialog.FileNames)
+                    {
+                        string fileName = Path.GetFileName(filePath);
+                        songList.Items.Insert(songList.SelectedIndex + 1, fileName);
+
+                        InsertAfter(currentlyPlayingSong, fileName, filePath);
+                    }
+                }
+            }
+        }
+        private void btn_Next_Click(object sender, EventArgs e)         // button Next
+        {
+            if (currentSong != null)
+            {
+                if (currentSong.next != null)
+                {
+                    currentSong = currentSong.next;
+                }
+                else
+                {
+                    currentSong = head;
+                }
+
+                PlaySelectedSong();
+            }
+        }
+        private void btn_Previous_Click(object sender, EventArgs e)         // button Previous
+        {
+            if (currentSong != null)
+            {
+                if (currentSong.prev != null)
+                {
+                    currentSong = currentSong.prev;
+                }
+                else
+                {
+                    currentSong = GetLastNode();
+                }
+
+                PlaySelectedSong();
+            }
+        }
+        private void btn_PlayPause_Click(object sender, EventArgs e)            // button play pause
+        {
+            if (currentSong != null)
+            {
+                if (waveOut == null)
+                {
+                    string path = currentSong.path;
+                    waveOut = new WaveOutEvent();
+                    audioFile = new AudioFileReader(path);
+                    waveOut.Init(audioFile);
+                    waveOut.Play();
+
+                    btn_PlayPause.Text = "Pause";
+                    StartTrackBarUpdateTimer();
+                }
+                else if (waveOut.PlaybackState == PlaybackState.Playing)
+                {
+                    waveOut.Pause();
+                    btn_PlayPause.Text = "Play";
+                }
+                else if (waveOut.PlaybackState == PlaybackState.Paused)
+                {
+                    waveOut.Play();
+                    btn_PlayPause.Text = "Pause";
+                }
+            }
+        }
+        private void btn_RemoveSong_Click(object sender, EventArgs e)           // button remove
+        {
+            if (songList.SelectedIndex >= 0)
+            {
+                string selectedSong = songList.SelectedItem.ToString();
+                Node selectedNode = FindNodeByData(selectedSong);
+
+                if (selectedNode != null)
+                {
+                    if (currentSong == selectedNode && waveOut != null)
+                    {
+                        StopPlayback();
+                    }
+
+                    if (selectedNode.prev != null)
+                    {
+                        selectedNode.prev.next = selectedNode.next;
+                    }
+                    else
+                    {
+                        head = selectedNode.next;
+                    }
+
+                    if (selectedNode.next != null)
+                    {
+                        selectedNode.next.prev = selectedNode.prev;
+                    }
+
+                    songList.Items.Remove(selectedSong);
+
+                    if (currentSong == selectedNode)
+                    {
+                        currentSong = null;
+                        UpdateCurrentSongDisplay();
+                    }
+                    lbl_CurrentPosition.Text = "00:00";
+                    lbl_TotalDuration.Text = "00:00";
+                    lbl_SongName.Text = "";
+                }
+            }
+
+        }
+
+
+        private void songList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (songList.SelectedIndex >= 0)
+            {
+                string selectedSong = songList.SelectedItem.ToString();
+                currentSong = FindNodeByData(selectedSong);
+                PlaySelectedSong();
+            }
+        }
+        public void InsertAtEnd(string fileName, string filePath)
+        {
+            Node newNode = new Node();
+            newNode.name = fileName;
+            newNode.path = filePath;
+            newNode.next = null;
+
+            if (head == null)
+            {
+                newNode.prev = null;
+                head = newNode;
+                tail = newNode;
+                return;
+            }
+
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        public void InsertAfter(string existingSong, String fileName, String filePath)
+        {
+            Node currentNode = head;
+
+            while (currentNode != null)
+            {
+                if (currentNode.name == existingSong)
+                {
+                    Node newNode = new Node();
+                    newNode.name = fileName;
+                    newNode.path = filePath;
+
+                    newNode.next = currentNode.next;
+                    if (currentNode.next != null)
+                    {
+                        currentNode.next.prev = newNode;
+                    }
+                    newNode.prev = currentNode;
+                    currentNode.next = newNode;
+
+                    break;
+                }
+
+                currentNode = currentNode.next;
+            }
+        }
+
+        private Node FindNodeByData(string targetData)              // find node
+        {
+            Node currentNode = head;
+            while (currentNode != null)
+            {
+                if (currentNode.name == targetData)
+                {
+                    return currentNode;
+                }
+                currentNode = currentNode.next;
+            }
+            return null;
+        }
+        private void UpdateCurrentSongDisplay()                     // update UI
+        {
+            if (currentSong != null)
+            {
+                lbl_SongName.Text = currentSong.name;
+
+                int songIndex = songList.Items.IndexOf(currentSong.name);
+                if (songIndex >= 0)
+                {
+                    songList.SelectedIndex = songIndex;
+                }
+            }
+            else
+            {
+                lbl_SongName.Text = "No song selected";
+                songList.SelectedIndex = -1;
+            }
+        }
+        private void StartTrackBarUpdateTimer()
+        {
+            trackBarUpdateTimer = new Timer();
+            trackBarUpdateTimer.Interval = 100;
+            trackBarUpdateTimer.Tick += TrackBarUpdateTimer_Tick;
+            trackBarUpdateTimer.Start();
+        }
+        private void TrackBarUpdateTimer_Tick(object sender, EventArgs e)
+        {
+            if (waveOut != null && audioFile != null)
+            {
+                int currentPosition = (int)(audioFile.Position / (double)audioFile.Length * trackBar.Maximum);
+                trackBar.Value = currentPosition;
+
+                TimeSpan totalDuration = audioFile.TotalTime;
+                TimeSpan currentDuration = TimeSpan.FromSeconds(currentPosition * totalDuration.TotalSeconds / trackBar.Maximum);
+                lbl_CurrentPosition.Text = $"{currentDuration:mm\\:ss}";
+                lbl_TotalDuration.Text = $"{totalDuration:mm\\:ss}";
+            }
+        }
+        private void StopPlayback()
+        {
+            if (waveOut != null)
+            {
+                waveOut.Stop();
+                waveOut.Dispose();
+                waveOut = null;
+            }
+
+            trackBar.Value = 0;
+        }
+        private Node GetLastNode()
+        {
+            if (head == null)
+            {
+                return null;
+            }
+
+            Node currentNode = head;
+            while (currentNode.next != null)
+            {
+                currentNode = currentNode.next;
+            }
+
+            return currentNode;
+        }
+        private void PlaySelectedSong()
+        {
+            if (currentSong != null)
+            {
+                StopPlayback();
+
+                string path = currentSong.path;
+                waveOut = new WaveOutEvent();
+                audioFile = new AudioFileReader(path);
+                waveOut.Init(audioFile);
+                waveOut.Play();
+
+                UpdateCurrentSongDisplay();
+
+                isPaused = false;
+
+                StartTrackBarUpdateTimer();
+            }
+        }
+
+
+    }
+}
